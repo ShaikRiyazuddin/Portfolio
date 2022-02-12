e252b7df5d2f46ead218c2c30d35ee1cb50ed305
ShaikRiyazuddin <shaikriyaz00723@gmail.com>
2022-02-12 13:25:58 +0530

    Made portfolio completely

---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./Contact.css";
+import EmailIcon from '@material-ui/icons/Email';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import CallIcon from '@material-ui/icons/Call';
+
+const Contact = () => {
+  return (
+    <div id="contact">
+      <h1 className="pt-3 text-center font-details-b pb-3">CONTACT ME</h1>
+      <Jumbotron className="contact-jumbotron">
+        <Row>
+          <Col className="d-flex justify-content-center flex-wrap">
+            <div className="m-2">
+              <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=riyaz.itguy@gmail.com" target="_blank" rel="noopener noreferrer">
+                <Button variant="outline-danger" title="riyaz.itguy@gmail.com">
+                  <EmailIcon/> Email Me
+                </Button>
+              </a>
+            </div>
+            <div className="m-2">
+              <a href="https://www.linkedin.com/in/riyazuddin-shaik" target="_blank" rel="noopener noreferrer">
+                <Button variant="outline-primary" title="Visit my LinkenIn">
+                  <LinkedInIcon/> LinkedIn
+                </Button>
+              </a>
+            </div>
+            
+            <div className="m-2">
+              <a href="https://github.com/ShaikRiyazuddin" target="_blank" rel="noopener noreferrer">
+                <Button variant="outline-dark" title="My other projects">
+                  <GitHubIcon/> GitHub
+                </Button>
+              </a>
+            </div>
+           
+            <div className="m-2">
+              <a href="https://twitter.com/RiyazItguy" target="_blank" rel="noopener noreferrer">
+                <Button variant="outline-info" title="Tweets are welcomed!">
+                  <TwitterIcon/> Twitter
+                </Button>
+              </a>
+            </div>
+            <div className="m-2">
+              <a href="https://www.facebook.com/shaik.riyaz.14606/" target="_blank" rel="noopener noreferrer">
+                <Button variant="outline-primary" title="Say hello on FB">
+                  <FacebookIcon/>FaceBook
+                </Button>
+              </a>
+            </div>
+          </Col>
+        </Row>
+        <div className = "about-desc-icon"> <CallIcon/>
+      <sapn className = "about-desc-span">+91 850 0591 806</sapn>
+      </div>
+      </Jumbotron>
+    </div>
+  
+  );
+};
+
+export default Contact;
---
+++ src/Pages/Projects/ProjectData.js
@@ -0,0 +1,122 @@
+import Project1 from "../../assets/img/projects/pantaloons.png";
+import Project2 from "../../assets/img/projects/Intershala.png";
+
+// import Project2 from "../../assets/img/projects/rememberTheMilk.png";
+// import Project3 from "../../assets/img/projects/amazon.png";
+// import Project4 from "../../assets/img/projects/ventureBeat.jpg";
+// import Project5 from "../../assets/img/projects/weather.png";
+// import Project6 from "../../assets/img/projects/student-database.png";
+// import Project7 from "../../assets/img/projects/todo-app.png";
+import HTML5 from "../../assets/img/skills/html.png";
+import CSS3 from "../../assets/img/skills/css3.svg";
+import SASS from "../../assets/img/skills/sass-1.svg";
+import BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
+import REACT from "../../assets/img/skills/react.svg";
+import REDUX from "../../assets/img/skills/redux.svg";
+import REACT_ROUTER from "../../assets/img/skills/react-router.svg";
+import MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+import REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
+import STYLED_COMPONENTS from "../../assets/img/skills/styled-components.svg";
+import NODE_JS from "../../assets/img/skills/nodejs.png";
+import EXPRESS from "../../assets/img/skills/express1.png";
+import DJANGO from "../../assets/img/skills/django.svg";
+import FLASK from "../../assets/img/skills/flask.svg";
+import MONGODB from "../../assets/img/skills/mongo.png";
+import POSTGRESQL from "../../assets/img/skills/postgresql.svg";
+import MSSQL from "../../assets/img/skills/mssql.svg";
+import GIT from "../../assets/img/skills/git-icon.svg";
+import HEROKU from "../../assets/img/skills/heroku.svg";
+import DIGITAOCEAN from "../../assets/img/skills/digital-ocean.svg";
+import GITHUB_PAGES from "../../assets/img/skills/github.svg";
+import JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+import TYPESCRIPT from "../../assets/img/skills/typescript.svg";
+import GOLANG from "../../assets/img/skills/go-6.svg";
+import PYTHON from "../../assets/img/skills/python.svg";
+
+export const projects = [
+  // {
+  //   project_name: "Ketto.org",
+  //   project_image: Project1,
+  //   Project_description:
+  //     "An online fundraising platform Created in under 4 days with a team of 3 members, Tech Stack: React, Redux, Hooks, Material UI, CSS",
+  //   github_link: "https://github.com/reemasahusbp5345/Ethane",
+  //   deployed_link: "https://ketto-clone.netlify.app",
+  //   technologies: [HTML5, CSS3, JAVASCRIPT, REACT, BOOTSTRAP, REACT_BOOTSTRAP],
+  // },
+  {
+    project_name: "Pantaloons Clone",
+    project_image: Project1,
+    Project_description:
+      "An online shopping Application, Implemented CRUD operations with HTML, CSS and JAVASCRIPT",
+    github_link: "https://github.com/ShaikRiyazuddin/Pantaloons.git",
+    deployed_link: "https://shaikriyazuddin.github.io/Pantaloons/",
+    technologies: [HTML5, CSS3, JAVASCRIPT],
+  },
+  {
+    project_name: "Intershala Clone",
+    project_image: Project2,
+    Project_description:
+      "An online shopping Application, Implemented CRUD operations with HTML, CSS, JAVASCRIPT, BOOTSTRAP, NODEJS, EXPRESS, MONGODB",
+    github_link: "https://github.com/ShaikRiyazuddin/Intershala-Clone.git",
+    deployed_link: "http://sampleclone-env.eba-9dmzyuuw.us-west-2.elasticbeanstalk.com/",
+    technologies: [HTML5, CSS3, JAVASCRIPT, BOOTSTRAP,NODE_JS,EXPRESS,MONGODB],
+  },
+  // {
+  //   project_name: "Amazon.com Clone",
+  //   project_image: Project3,
+  //   Project_description:
+  //     "Cloned amazon.com website perfectly, a user can signup and signin with google and add items to cart. Technologies used are React, Firebase",
+  //   github_link: "https://github.com/faizitguy/amazon-clone/tree/master",
+  //   deployed_link: "https://clone-2be4b.web.app/",
+  //   technologies: [HTML5, CSS3, JAVASCRIPT, REACT, MATERIALUI],
+  // },
+  // {
+  //   project_name: "VentureBeat.com Clone",
+  //   project_image: Project4,
+  //   Project_description:
+  //     "venturebeat.com is one of the famous Tech Blog. Cloned it under 3 days with a team of 4 people. Technologies used here are Bootstrap, Material-UI, React, Redux, and React-Redux.",
+  //   github_link: "https://github.com/Gaursafal/VentureBeat-Clone",
+  //   deployed_link:
+  //     "https://drive.google.com/file/d/1TO9um9tHYnPLtMGEj0Lg2vyIP9Ea6O26/view?usp=sharing",
+  //   technologies: [HTML5, CSS3, JAVASCRIPT, REACT, MATERIALUI],
+  // },
+  // {
+  //   project_name: " MERN Student Database",
+  //   project_image: Project6,
+  //   Project_description:
+  //     "Developed Full Stack MERN student database application where we can add, edit, delete students. Used nodejs, expressjs and mongoDB in the backend",
+  //   github_link: "https://github.com/faizitguy/student-database",
+  //   deployed_link:
+  //     "https://drive.google.com/file/d/1fndDOeI-J9qzhdqlsxKoCvc_nic0CWMV/view",
+  //   technologies: [
+  //     CSS3,
+  //     JAVASCRIPT,
+  //     REACT,
+  //     MATERIALUI,
+  //     NODE_JS,
+  //     EXPRESS,
+  //     MONGODB,
+  //   ],
+  // },
+
+  // {
+  //   project_name: "Todo Application",
+  //   project_image: Project7,
+  //   Project_description:
+  //     "A basic todo application where user can add, edit, delete Tasks.Technologies used are ReactJS, MaterialUI, Redux",
+  //   github_link: "https://github.com/faizitguy/todo-app",
+  //   deployed_link: "https://friendly-easley-8572ad.netlify.app/",
+  //   technologies: [HTML5, CSS3, JAVASCRIPT, REACT, MATERIALUI, REDUX],
+  // },
+
+  // {
+  //   project_name: "Weather API",
+  //   project_image: Project5,
+  //   Project_description:
+  //     "Weather Search App where anyone can get the weather status by searching their city name. The technologies used are HTML, CSS & Javascript.",
+  //   github_link: "https://github.com/faizitguy/masai-sprint-3",
+  //   deployed_link:
+  //     "https://drive.google.com/file/d/1N-MqzNqIaAsNUOvWGV5uFISiTWU_Xiiz/view?usp=sharing",
+  //   technologies: [HTML5, CSS3, JAVASCRIPT, REACT, MATERIALUI],
+  // },
+];
